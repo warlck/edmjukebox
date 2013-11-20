837cf291d93124abf852fb8535d65e6e44d1e334
warlck <addick@me.com>
2013-11-20 22:22:59 +0800

    Disable assets initialize of precompile

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+config.assets.initialize_on_precompile = false
 module Edmplayer
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
