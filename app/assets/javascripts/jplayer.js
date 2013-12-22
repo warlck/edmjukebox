bea2bcc13fe981948eaee8c115128432371117c2
warlck <addick@me.com>
2013-12-22 10:45:48 +0800

    Edit artist show page, add jplayer

---
+++ app/assets/javascripts/jplayer.js
@@ -0,0 +1,11 @@
+//  $(document).ready(function(){
+//       $("#jquery_jplayer_1").jPlayer({
+//         ready: function () {
+//            $(this).jPlayer("setMedia", {
+//                 m4a: "http://media.rawvoice.com/officialaviciipodcast/media2-av.podtree.com/media/podcast/AVICII_LEVELS_-_EPISODE_017.m4a",
+//             }) // auto play
+//         },
+//         swfPath: ".",
+//         supplied: "m4a"
+//       }); 
+// });
