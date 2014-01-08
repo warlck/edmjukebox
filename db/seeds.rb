ac2f9c8b9268e9033edf5a514ef71b6e7d895586
warlck <addick@me.com>
2014-01-08 12:53:17 +0800

    add seeding instructions to db/seed to fill the database with artists and their podcasts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+feed_addresses = [
+	       "http://podcast.djhardwell.com/podcast.xml", 
+	       "http://feeds.djpod.com/corstenscountdown",
+	       "http://officialaviciipodcast.podtree.com/feed/podcast/", 
+	       "http://trashedradiopodcast.libsyn.com/rss", 
+	       "http://ericprydz.podtree.com/feed/podcast/", 
+	       "http://promodj.com/Andrew-Rayel/podcast.xml", 
+	       "http://quintino.podtree.com/feed/podcast/",
+	       "http://www.tatw.co.uk/podcast.xml",
+	       "http://www.paulvandyk.com/podcast.php",
+	       "http://oakenfold.libsyn.com/rss", 
+	       "http://podcast.arunamusic.com/ap.xml", 
+	       "http://podcast.daftasticradio.com/podcast.xml", 
+	       "http://podcast.wandwmusic.nl/podcast.php", 
+	       "http://www.protocol-radio.com/podcast/PRRADIO.xml", 
+	       "http://feeds.feedburner.com/bingoplayerspodcast/hysteriaradio", 
+	       "http://feeds.feedburner.com/tiestos_club_life", 
+	       "http://feeds.feedburner.com/MarkusSchulzGlobalDJBroadcast"]
+
+
+feed_addresses.each do  |address|
+	Artist.create_artist address
+end
