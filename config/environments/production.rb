49bffd5d7ea70016eefaa748e76234f98fadc207
warlck <addick@me.com>
2014-01-11 16:20:37 +0800

    Edit production logger to show sql statements and params

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Edmplayer::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-   config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new)
+    config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production
    config.cache_store = :dalli_store
